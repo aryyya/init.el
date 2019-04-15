a25b3b84d8da4129de25d8527c6a42ac1f6999ce
aryyya.xyz@gmail.com <29365565+aryyya@users.noreply.github.com>
2019-04-15 13:48:15 -0700

    Initial commit.

---
+++ init.el
@@ -0,0 +1,90 @@
+;; aryyya's emacs configuration
+;;
+;; to install packages, use:
+;;   (package-refresh-contents)
+;;   (package-install-selected-packages)
+;;
+
+(defun init-emacs ()
+
+  ;; initialize the user interface
+  (init-ui)
+
+  ;; initialize stuff that doesn't fit anywhere else
+  (init-misc)
+
+  ;; initialize package repositories
+  (init-package-repos)
+
+  ;; initialize packages
+  (init-packages)
+
+  )
+
+(defun init-ui ()
+  
+  ;; configure settings for gui mode
+  (if (display-graphic-p)
+      (progn
+	
+	;; disable gui features
+	(toggle-scroll-bar -1)
+	(tool-bar-mode -1)
+
+	;; fix window gaps
+	(setq frame-resize-pixelwise t)
+
+	;; set font face
+	(set-default-font "Menlo 14")
+
+	;; show end of files in fringe
+	(toggle-indicate-empty-lines)
+	
+	)
+    )
+  )
+
+(defun init-misc ()
+
+  ;; add system paths
+  (add-to-list 'exec-path "/usr/local/bin")
+
+  )
+
+(defun init-package-repos ()
+
+  ;; package initialize before configurations of installed packages
+  (package-initialize)
+
+  ;; add melpa repo
+  (require 'package)
+  (add-to-list 'package-archives
+	       '("melpa" . "https://melpa.org/packages/"))
+
+  )
+
+(defun init-packages ()
+
+  ;; rainbow-delimiters
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+  ;; paredit
+  (add-hook 'clojure-mode-hook #'paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook #'paredit-mode)
+
+  ;; show-paren
+  (add-hook 'prog-mode-hook #'show-paren-mode)
+
+  )
+
+;; execute configuration
+(init-emacs)
+
+;; emacs bookkeeping (do not hand modify the file past here)
+(custom-set-variables
+ '(package-selected-packages
+   (quote
+    (rainbow-delimiters treemacs transpose-frame paredit cider))))
+(custom-set-faces
+ )
+
