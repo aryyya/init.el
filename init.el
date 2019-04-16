deb0f0cf35eb34051a6dfce8efa869360a6fb9f5
aryyya <29365565+aryyya@users.noreply.github.com>
2019-04-15 23:13:26 -0700

    Remove unnecessary package.

--- init.el
+++ init.el
@@ -112,7 +112,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (haskell-mode rainbow-delimiters treemacs transpose-frame paredit cider))))
+    (haskell-mode rainbow-delimiters treemacs paredit cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
