d451ad8c66ace7518bb14f1f031edbc8133cc079
aryyya <29365565+aryyya@users.noreply.github.com>
2019-04-16 20:46:33 -0700

    Use rainbow parens in CIDER.

--- init.el
+++ init.el
@@ -84,6 +84,7 @@
 
   ;; rainbow-delimiters
   (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
 
   ;; paredit
   (add-hook 'clojure-mode-hook #'paredit-mode)
